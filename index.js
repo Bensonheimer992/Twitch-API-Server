5af8282f193856917778ad1ac48d3f8fda356e30
Bensonheimer992 <101340634+Bensonheimer992@users.noreply.github.com>
2024-04-28 13:50:12 +0200

    Added some other Parameters

--- index.js
+++ index.js
@@ -5,28 +5,41 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT;
 
-const client_id = process.env.CLIENT_ID;
-const client_secret = process.env.CLIENT_SECRET;
+const twitch_client_id = process.env.CLIENT_ID;
+const twitch_client_secret = process.env.CLIENT_SECRET;
 
 app.get('/api/:name', async (req, res) => {
     try {
         const name = req.params.name;
 
-        axios.post("https://id.twitch.tv/oauth2/token?client_id=" + client_id + "&client_secret=" + client_secret + "&grant_type=client_credentials").then(response => {
+        axios.post("https://id.twitch.tv/oauth2/token?client_id=" + twitch_client_id + "&client_secret=" + twitch_client_secret + "&grant_type=client_credentials").then(response => {
             let token = response.data.access_token
-
+            
             axios.get("https://api.twitch.tv/helix/streams?user_login=" + name, {
-                headers: {
-                    Authorization: "Bearer " + token,
-                    "Client-Id": client_id
-                }
+            headers: {
+                Authorization: "Bearer " + token,
+                "Client-Id": twitch_client_id
+            }
             }).then(livedata => {
+                axios.get("https://api.twitch.tv/helix/users?login=" + name, {
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        "Client-Id": twitch_client_id
+                    }
+                }).then(getuserdata => {
                 let data = livedata.data
+                let userdata = getuserdata.data.data[0]
                 if (data.data.length > 0) {
-                    res.json({ live: true, username: data.data[0].user_name, title:data.data[0].title, game: data.data[0].game_name, view_count: data.data[0].viewer_count, language: data.data[0].language })
+                    const thumbnailbefore = data.data[0].thumbnail_url
+                    const thumbnailUrl = thumbnailbefore.replace('{width}', '1920').replace('{height}', '1080');
+
+                    res.json({ live: true, username: data.data[0].user_name, user_description: userdata.description, user_profilepicture: userdata.profile_image_url, offline_banner: userdata.offline_image_url, account_created: userdata.created_at,  title:data.data[0].title, thumbnail: thumbnailUrl,  game: data.data[0].game_name, view_count: data.data[0].viewer_count, language: data.data[0].language })
+                    console.log("New Twitch API Request with live: true")
                 } else {
-                    res.json({ live: false })
+                    res.json({ live: false, user_profilepicture: userdata.profile_image_url, offline_banner: userdata.offline_image_url })
+                    console.log("New Twitch API Request with live: false")
                 }
+                });
             });
         });
     } catch (error) {
